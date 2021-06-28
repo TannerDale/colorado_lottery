4fcc72d0e6688ea6dc1f874c77a7d5634049a01d
Tanner Dale <tanner@tannerdale.com>
2021-06-28 15:33:29 -0500

    Rename to game, make game initializing tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,29 @@
+require './lib/game'
+
+RSpec.descride Game do
+  describe 'name' do
+    pick_4 = Game.new('Pick 4', 2)
+
+    it 'has a name' do
+      expect(pick_4.name).to eq('Pick 4')
+    end
+  end
+
+  describe 'cost' do
+    pick_4 = Game.new('Pick 4', 2)
+
+    it 'has a cost' do
+      expect(pick_4.cost).to eq(2)
+    end
+  end
+
+  describe '#national drawing' do
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 4, true)
+
+    it 'can recognize national game' do
+      expect(pick_4.national_drawing?).to be(false)
+      expect(mega_millions.national_drawing?).to be(true)
+    end
+  end
+end
