b6b439b447fdf9261db60dcb1413715b818443ab
Tanner Dale <tanner@tannerdale.com>
2021-06-28 15:58:51 -0500

    Add iteration 1 contestant class

---
+++ lib/contestant.rb
@@ -0,0 +1,26 @@
+require './lib/game'
+
+class Contestant
+  attr_reader :age, :state_of_residence, :spending_money, :game_interests
+
+  def initialize(args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @age = args[:age]
+    @state_of_residence = args[:state_of_residence]
+    @spending_money = args[:spending_money]
+    @game_interests = []
+  end
+
+  def full_name
+    @first_name + ' ' + @last_name
+  end
+
+  def out_of_state?
+    @state_of_residence != 'CO'
+  end
+
+  def add_game_interest(game)
+    @game_interests << game
+  end
+end
